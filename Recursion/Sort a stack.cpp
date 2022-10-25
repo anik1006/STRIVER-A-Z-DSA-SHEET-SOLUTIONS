f66857ecce6e9a795c5a7a31378e92627ec1290b
Aniruddh Kumar <89256634+anik1006@users.noreply.github.com>
2022-10-25 21:49:10 +0530

    Update Sort a stack.cpp

--- Recursion/Sort a stack.cpp
+++ Recursion/Sort a stack.cpp
@@ -1,14 +1,4 @@
 
-/*The structure of the class is
-class SortedStack{
-public:
-	stack<int> s;
-	void sort();
-};
-*/
-
-/* The below method sorts the stack s 
-you are required to complete the below method */
 void print(priority_queue<int>&ans){
     if(ans.empty()) return;
     cout<<ans.top()<<" ";
